4e1d0dbe66c992c462b85818509b5455560ebcb0
nathan-dcruz <contactnathanldc@gmail.com>
2024-03-10 20:08:34 -0700

    finish connect 4 game logic implementation

---
+++ connect4_board/c4.c
@@ -0,0 +1,304 @@
+/*
+ * c4.c
+ *
+ *  Created on: Mar 7, 2024
+ *      Author: Nathan D'Cruz
+ *      Adapted from connec4.py by Henry Sun
+ */
+
+#include "c4.h"
+
+// GLOBALS DECLARATION
+struct C4_PLAYER {
+    player_t* opponent;
+    uint8_t id;
+}; // player_t
+
+struct C4_GAME_OBJ {
+    // GAME STATE
+    player_t*   winner;
+    uint8_t     status;
+
+    // BOARD STATE
+    uint8_t     board[BOARD_HEIGHT][BOARD_WIDTH];
+    uint8_t     lowest_empty_col_slot[BOARD_WIDTH];
+    long        num_moves;
+
+    // PLAYER STATE
+    player_t    player1;
+    player_t    player2;
+    player_t*   player_turn;
+
+}; // connect4_t
+
+static connect4_t GAME;
+
+static bool GAME_INIT = false;
+static const int  MAX_MOVES = BOARD_WIDTH * BOARD_HEIGHT;
+
+static uint8_t last_player_id = 0;
+static player_t TIE_PLAYER;
+static player_t NO_PLAYER;
+
+// PRIVATE METHODS
+static int imax(int a, int b) { return (a == b ? a : (a > b ? a : b)); }
+static int imin(int a, int b) { return (a == b ? a : (a < b ? a : b)); }
+
+static void fmt_err_c4(const char* err_msg) {
+    // Enforce max length
+    char err_msg_buf[MAX_ERR_LEN];
+    strncpy(err_msg_buf, err_msg, MAX_ERR_LEN);
+
+    // Format message
+    char fmt_err_msg[MAX_ERR_LEN + 3]; // Accommodate \n\r\0
+    sprintf(fmt_err_msg, "%s\n\r\0", err_msg);
+
+    // Write to UART
+    Report(fmt_err_msg);
+}
+
+static void init_player(player_t* player, player_t* opponent) {
+    player->opponent = opponent;
+    last_player_id++;
+    player->id = last_player_id;
+}
+
+static void init_c4t() {
+    // Initialize player state
+
+    // Dummy players for special player states
+    NO_PLAYER.id = 0;           // For when player is "null"
+    NO_PLAYER.opponent = NULL;
+    TIE_PLAYER.id = 255;        // For when the game is a tie
+    TIE_PLAYER.opponent = NULL;
+
+    init_player(&GAME.player1, &GAME.player2);
+    init_player(&GAME.player2, &GAME.player1);
+
+    GAME.player_turn = &NO_PLAYER;
+
+    // Initialize board
+    int col;
+    int row;
+    for (col = 0; col < BOARD_WIDTH; col++) {
+        for (row = 0; row < BOARD_HEIGHT; row++) {
+            GAME.board[row][col] = 0;
+        }
+        GAME.lowest_empty_col_slot[col] = BOARD_HEIGHT - 1;
+    }
+    GAME.num_moves = 0;
+
+
+    // Initialize game state
+    GAME.winner = &NO_PLAYER;
+    GAME.status = GAME_INACTIVE;
+    GAME.status = GAME_INACTIVE;
+    GAME_INIT = true;
+}
+
+static bool set_winner(bool is_tie){
+    if (!(GAME.status == GAME_ACTIVE)) {
+        fmt_err_c4("Cannot set winner: no active game");
+        return true;
+    }
+
+    GAME.winner = is_tie ? &TIE_PLAYER : GAME.player_turn;
+
+    // TODO: winner post-logic
+    //...
+
+    return false;
+}
+
+static bool do_move(player_t* player, uint8_t selected_column) {
+    uint8_t row = GAME.lowest_empty_col_slot[selected_column];
+
+    uint8_t cell = GAME.board[row][selected_column];
+    if (!(cell == CELL_EMPTY)) {
+        fmt_err_c4("Cannot do move: cell already in use");
+        return true;
+    }
+
+    GAME.board[row][selected_column] = player->id;
+    GAME.lowest_empty_col_slot[selected_column]--;
+    GAME.num_moves++;
+
+    return false;
+}
+
+static void turn_transition() {
+    GAME.player_turn = GAME.player_turn->opponent;
+}
+
+static uint8_t play_turn() {
+    uint8_t selected_column; // TODO: poll for input
+                             // Listen for game quit
+    bool valid_move = false;
+
+    while (!valid_move) {
+        selected_column = 2; // DUMMY VALUE until above TODO satisfied
+
+        // use unsigned negative overflow to detect when the column is full
+        valid_move = !(GAME.lowest_empty_col_slot[selected_column] > BOARD_HEIGHT - 1);
+    }
+
+    do_move(GAME.player_turn, selected_column);
+    return selected_column;
+}
+
+bool check_board_full() { return (GAME.num_moves >= MAX_MOVES); }
+
+// PUBLIC METHODS
+connect4_t get_game_c4(){
+    if (!GAME_INIT) { init_c4t(); }
+   return GAME;
+}
+
+bool game_active_c4() { return ( GAME_INIT && (GAME.status == GAME_ACTIVE) ); }
+bool game_initalized_c4() { return (GAME_INIT); }
+
+bool start_game_c4() {
+    if (!GAME_INIT) {
+        fmt_err_c4("Game has not been initialized. Please call get_game_c4() first.");
+        return true;
+    } else if (game_active_c4()) {
+        fmt_err_c4("There is already a game in progress.");
+        return true;
+    }
+
+    GAME.status = GAME_ACTIVE;
+
+    GAME.player_turn = &GAME.player1;
+    bool first_turn = true;
+
+    // Game loop
+    bool game_over = false;
+    bool is_tie = false;
+
+    while (!game_over) {
+        if (!first_turn) {
+            turn_transition();
+        }
+
+        uint8_t last_move = play_turn();
+        first_turn = false;
+
+        int game_over_status = check_game_over_c4(last_move, GAME.player_turn);
+        game_over = game_over_status > 0;
+        is_tie = game_over_status > 1;
+    }
+
+    if (game_over) {
+        set_winner(is_tie);
+        GAME.status = GAME_OVER;
+    }
+
+    return false;
+}
+
+uint8_t check_game_over_c4(uint8_t move_col, player_t* last_move_player) {
+    uint8_t move_row = GAME.lowest_empty_col_slot[move_col] + 1;
+
+    uint8_t min_row_index = imax(move_col - 3, 0);
+    uint8_t max_row_index = imin(move_col + 3, BOARD_HEIGHT - 1);
+    uint8_t max_col_index = imax(move_row - 3, 0);
+    uint8_t min_col_index = imin(move_row + 3, BOARD_WIDTH - 1);
+
+    int min_left_diag[2] = {imax(move_col - 3, move_col), imin(move_row + 3, BOARD_WIDTH - 1)};
+    int max_left_diag[2] = {imin(move_col + 3, BOARD_HEIGHT - 1), imax(move_row - 3, 0)};
+    int min_right_diag[2] = {imin(move_col + 3, move_col), imin(move_row + 3, BOARD_WIDTH - 1)};
+    int max_right_diag[2] = {imax(move_col - 3, 0), imax(move_row - 3, 0)};
+
+    // Horizontal
+    int count = 0;
+    int s;
+    for(s = min_row_index; s <= max_row_index; s++) {
+        int pos_owner = GAME.board[move_row][s];
+
+        if (pos_owner == last_move_player->id) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count == 4) {
+            // TODO: draw line
+            return 1;
+        }
+    }
+
+    // Vertical
+    count = 0;
+    for(s = max_col_index; s <= min_col_index; s++) {
+        int pos_owner = GAME.board[s][move_col];
+
+        if (pos_owner == last_move_player->id) {
+            count++;
+        } else {
+            count = 0;
+        }
+
+        if (count == 4) {
+            // TODO: draw line
+            return 1;
+        }
+    }
+
+    // Left diagonal
+    int row = move_row;
+    int col = move_col;
+    count = 0;
+    int up = 0; // unused??
+    int down_count = 0; // used for drawing the line
+
+    while (row > -1 && col > -1 && GAME.board[row][col] == last_move_player->id) {
+        count++;
+        row--;
+        col--;
+    }
+    down_count = count;
+    row = move_row + 1;
+    col = move_col + 1;
+
+    while (row < BOARD_WIDTH && col < BOARD_HEIGHT && GAME.board[row][col] == last_move_player->id) {
+        count++;
+        row++;
+        col++;
+    }
+    if (count >= 4) {
+        // TODO: draw line
+        return 1;
+    }
+
+    // Right diagonal
+    row = move_row;
+    col = move_col;
+    count = 0;
+    up = 0;
+    down_count = 0;
+
+    while (row > -1 && col > -1 && GAME.board[row][col] == last_move_player->id) {
+        count++;
+        row++;
+        col--;
+    }
+    down_count = count;
+    row = move_row + 1;
+    col = move_col + 1;
+
+    while (row < BOARD_WIDTH && col < BOARD_HEIGHT && GAME.board[row][col] == last_move_player->id) {
+        count++;
+        row--;
+        col++;
+    }
+    if (count >= 4) {
+        // TODO: draw line
+        return 1;
+    }
+
+    if (check_board_full()) {
+        return 2;
+    }
+
+    return 0;
+}
---
+++ connect4_board/c4.h
@@ -0,0 +1,123 @@
+/*
+ * c4.h
+ *
+ *  Created on: Mar 7, 2024
+ *      Author: Nathan D'Cruz
+ */
+
+#ifndef C4_H_
+#define C4_H_
+
+#include <math.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+#include "uart_if.h"
+
+// MACROS
+// OPTIONS
+#define BOARD_WIDTH     6 // MAX 255
+#define BOARD_HEIGHT    7 // MAX 255
+
+// Game state
+#define GAME_INACTIVE   0
+#define GAME_ACTIVE     1
+#define GAME_OVER       2
+
+// Board state
+#define CELL_EMPTY      0
+
+// Null pointer
+#define NULL 0
+// Max error string length
+#define MAX_ERR_LEN 1024
+
+
+// "CLASSES" LOL
+/*
+ * A struct to encapsulate the game state
+ */
+struct C4_GAME_OBJ;
+struct C4_PLAYER;
+
+// Alias `struct C4_GAME_OBJ` to `connect4_t`
+typedef struct C4_GAME_OBJ  connect4_t;
+
+// Alias `struct C4_PLAYER` to `connect4_t`
+typedef struct C4_PLAYER    player_t;
+
+// PRIVATE MEMBERS
+
+/*
+ * Standard integer max/min methods
+ */
+static int imax(int a, int b);
+static int imin(int a, int b);
+
+/*
+ * Sent a formatted error message over UART
+ * Messages must be MAX_ERR_LEN char or less in length
+ */
+static void fmt_err_c4();
+
+
+/*
+ * Initialize a C4_GAME_OBJ
+ */
+static void init_c4t();
+
+/*
+ * Modify game state for a move
+ */
+static bool do_move(player_t* player, uint8_t selected_column);
+
+/*
+ * Transition from player 1 turn to player 2
+ */
+static void turn_transition();
+
+/*
+ * Play a turn
+ */
+static uint8_t play_turn();
+
+// PUBLIC MEMBERS
+
+/*
+ * Obtain Connect 4 singleton. Initialize if unavailable.
+ */
+connect4_t  get_game_c4();
+
+/*
+ * Player accessors
+ */
+player_t    get_player1();
+player_t    get_player2();
+
+/*
+ * Check if a game is initialized.
+ */
+bool        game_initialized_c4();
+
+/*
+ * Check if a game is active.
+ * Returns 0 for INACTIVE, 1 for ACTIVE
+ */
+bool        game_active_c4();
+
+/*
+ * Start a game of Connect 4
+ */
+bool        start_game_c4();
+
+/*
+ * Check the end-conditions for the game.
+ * Returns 1 if there is a winner
+ * Returns 2 if there is a tie
+ * Returns 0 otherwise
+ */
+uint8_t     check_game_over_c4(uint8_t move_col, player_t* last_move_player);
+
+
+#endif /* C4_H_ */
